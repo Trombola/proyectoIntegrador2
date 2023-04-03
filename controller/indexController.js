a0fd4262f693472709cee057cd036c8b444b787b
Trombola <trombola@udesa.edu.ar>
2023-04-03 10:25:16 -0300

    trabajamos en el estilo de los formularios

--- controller/indexController.js
+++ controller/indexController.js
@@ -3,33 +3,33 @@ const data = require('../data/data')
 const controller = {
     index: function (req, res) {
         if(req.params.si){
-            res.render('index', {lsProd: data.productos, logueado: 'si', usuario: data.usuario.usuario})
+            return res.render('index', {lsProd: data.productos, logueado: 'si', usuario: data.usuario.usuario})
         }
         else{
-            res.render('index', {lsProd: data.productos, logueado: 'no'})
+           return res.render('index', {lsProd: data.productos, logueado: 'no'})
         }
         
     },
     login: function (req, res) {
-        res.render('login')
+        return res.render('login')
     },
     register: function (req, res) {
-        res.render('register')
+        return res.render('register')
     },
     searchResults: function (req, res) {
-        res.render('search-results')
+        return res.render('search-results')
     },
     productAdd: function (req, res) {
-        res.render('product-add', {usuario: data.usuario.usuario})
+        return res.render('product-add', {usuario: data.usuario.usuario})
     },
     perfil: function (req, res) {
-        res.render('profile', {usuario: data.usuario.usuario, foto: data.usuario.fto,mail: data.usuario.mail})
+        return res.render('profile', {usuario: data.usuario.usuario, foto: data.usuario.fto,mail: data.usuario.mail})
     },
     editar_perfil: function (req, res) {
-        res.render('profile-edit', {usuario: data.usuario.usuario})
+        return res.render('profile-edit', {usuario: data.usuario.usuario})
     },
     product: function (req, res) {
-        res.render('product', {producto: data.productos[2], comentarios: data.comentarios})
+        return res.render('product', {producto: data.productos[2], comentarios: data.comentarios})
     },
 
 }
