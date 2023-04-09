bbc09053908a39c7d0189f4d572bf2b039f305d4
ezebilinkis <ebilinkis@udesa.edu.ar>
2023-04-09 16:00:34 -0300

    controladores divididos en user, products e index terminados

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,6 +1,6 @@
 const data = require('../data/data')
 
-const controller = {
+const indexController = {
     index: function (req, res) {
         if(req.params.si){
             return res.render('index', {lsProd: data.productos, logueado: 'si', usuario: data.usuario.usuario})
@@ -8,30 +8,7 @@ const controller = {
         else{
            return res.render('index', {lsProd: data.productos, logueado: 'no'})
         }
-        
-    },
-    login: function (req, res) {
-        return res.render('login')
-    },
-    register: function (req, res) {
-        return res.render('register')
-    },
-    searchResults: function (req, res) {
-        return res.render('search-results', {lsProd: data.productos})
-    },
-    productAdd: function (req, res) {
-        return res.render('product-add', {usuario: data.usuario.usuario})
-    },
-    perfil: function (req, res) {
-        return res.render('profile', {usuario: data.usuario.usuario, foto: data.usuario.fto, mail: data.usuario.mail, perfil: data.productos})
-    },
-    editar_perfil: function (req, res) {
-        return res.render('profile-edit', {usuario: data.usuario.usuario})
-    },
-    product: function (req, res) {
-        return res.render('product', {producto: data.productos[2], comentarios: data.comentarios})
-    },
-
+    }
 }
 
-module.exports = controller;
+module.exports = indexController;
